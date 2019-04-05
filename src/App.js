fe6a4ef36312161dcf53c2afa4885e0c3992da7d
Adrian Aymont <aaymont@gmail.com>
2019-04-04 21:59:07 -0400

    Configured Amplify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 
 class App extends Component {
   render() {
@@ -8,4 +9,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
