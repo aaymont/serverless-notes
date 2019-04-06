8efe31b7d4dd25f27613fc2dec1e6b783240661a
Adrian Aymont <aaymont@gmail.com>
2019-04-06 11:06:41 -0400

    Add new note to GraphQL API and update notes list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
+import { createNote } from './graphql/mutations';
 
 class App extends Component {
   state = {
-    notes: [{
-      id: 1,
-      note: "Hello World"
-    }]
-  }
+    note: "",
+    notes: []
+  };
+
+  handleChangeNote = event => this.setState({ note: event.target.value });
+
+  handleAddNote = async event => {
+    event.preventDefault();
+    const { note, notes } = this.state;
+    const input = { note };
+    const result = await API.graphql(graphqlOperation(createNote, { input }));
+    const newNote = result.data.createNote;
+    const updatedNotes = [newNote, ...notes];
+    this.setState({ notes: updatedNotes, note: "" });
+  };
 
   render() {
-    const { notes } = this.state;
+    const { notes, note } = this.state;
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-l">Amplify Notetaker</h1>
         {/* Note form */}
-        <form className="mb3">
+        <form className="mb3" onSubmit={this.handleAddNote}>
           <input type="text" className="pa2 f4"
             placeholder="Write your note"
+            onChange={this.handleChangeNote}
+            value={note}
           />
           <button className="pa2 f4"
             type="submit">
